8d733e8b33d5850fc21fb53ffcbf36d4ab0d0185
jajorda2 <jajorda2@me.com>
2018-01-14 14:00:07 -0500

    changesportcointime

--- src/base58.h
+++ src/base58.h
@@ -277,7 +277,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 19, // addresses start with L
+        PUBKEY_ADDRESS = 11, // addresses start with C
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
